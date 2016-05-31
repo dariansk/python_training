82683733b52969385262423c78eae3939f60d5d2
dariansk <103203@gmail.com>
2016-05-31 11:25:58 +0600

    Выделен класс для работы с контактами

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,22 +1,11 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+class ContactHelper:
 
 
-class Application:
+    def __init__(self, app):
+        self.app = app
 
-    def __init__(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys("\\undefined")
-
-    def create_new_contact(self, contact):
-        wd = self.wd
+    def create(self, contact):
+        wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
@@ -76,25 +65,3 @@ class Application:
         wd.find_element_by_name("notes").send_keys(contact.notes)
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         wd.find_element_by_link_text("home").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-
-    def destroy(self):
-        self.wd.quit()
-
-
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import pytest
-from fixture.application2 import Application
+from fixture.application import Application
 from model.contact import Contact
 
 
@@ -12,13 +12,14 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_new_contact(Contact(firstname="ytrewq", middlename="rewq", lastname="rewq", nickname="ertyu", title="mr", company="wert", address="wertweryttwy", home="23456", mobile="236",
-                                work_phone="256546", fax="255", birth_year="1234", ayear="1334", address2="46256", phone2="sfdgsdfhg", notes="sdfgh"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="ytrewq", middlename="rewq", lastname="rewq", nickname="ertyu", title="mr", company="wert", address="wertweryttwy", home="23456", mobile="236",
+                               work_phone="256546", fax="255", birth_year="1234", ayear="1334", address2="46256", phone2="sfdgsdfhg", notes="sdfgh"))
+    app.session.logout()
+
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_new_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home="", mobile="",
-                                work_phone="", fax="", birth_year="", ayear="", address2="", phone2="", notes=""))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home="", mobile="",
+                               work_phone="", fax="", birth_year="", ayear="", address2="", phone2="", notes=""))
+    app.session.logout()
