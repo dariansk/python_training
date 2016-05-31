da56e1c12e576abcdac36f9d87c73fd0751123f2
dariansk <103203@gmail.com>
2016-05-31 14:15:01 +0600

    Тесты с поправками

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,13 +2,6 @@
 from model.group import Group
 
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
-
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="qeqwer", header="qwerqwe", footer="qwerqwer"))
