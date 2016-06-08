83c880d4480182f9deb3564924010816890cb719
dariansk <103203@gmail.com>
2016-06-08 13:44:54 +0600

    Сделана интеллектуальная фикстура

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,14 +4,10 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="ytrewq", middlename="rewq", lastname="rewq", nickname="ertyu", title="mr", company="wert", address="wertweryttwy", home="23456", mobile="236",
                                work_phone="256546", fax="255", birth_year="1234", ayear="1334", address2="46256", phone2="sfdgsdfhg", notes="sdfgh"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home="", mobile="",
                                work_phone="", fax="", birth_year="", ayear="", address2="", phone2="", notes=""))
-    app.session.logout()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,7 +2,5 @@ from model.contact import Contact
 
 
 def test_modify_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="3737675676", middlename="", lastname="", nickname="", title="", company="", address="", home="", mobile="",
                                work_phone="", fax="", birth_year="", ayear="", address2="", phone2="", notes=""))
-    app.session.logout()
