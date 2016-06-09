91c5eb5391496e8520ffc3b7110410f4d74a254a
dariansk <103203@gmail.com>
2016-06-09 10:50:43 +0600

    Добавлены проверка и обеспечение предусловий в тесты модификации

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,9 +2,13 @@ from model.contact import Contact
 
 
 def test_modify_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="testcontact"))
     app.contact.modify_first_contact(Contact(firstname="New name"))
 
 
 def test_modify_contact_lastname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(lastname="testcontact"))
     app.contact.modify_first_contact(Contact(lastname="New lastname"))
 
