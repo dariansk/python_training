e31d54f44eec16e472e086a66d86559675376666
dariansk <103203@gmail.com>
2016-06-29 19:18:46 +0600

    Добавлен тест на проверку информации о контакте

--- fixture/contact.py
+++ fixture/contact.py
@@ -92,12 +92,13 @@ class ContactHelper:
             self.contact_cache = []
             for row in wd.find_elements_by_name("entry"):
                 cells = row.find_elements_by_tag_name("td")
-                text1 = cells[1].text
-                text2 = cells[2].text
+                lastname = cells[1].text
+                firstname = cells[2].text
+                address = cells[3].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
                 all_phones = cells[5].text
                 all_emails = cells[4].text
-                self.contact_cache.append(Contact(firstname=text2, lastname=text1, id=id,
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id, address=address,
                                                   all_phones_from_home_page=all_phones, all_emails_from_home_page=all_emails))
         return list(self.contact_cache)
 
@@ -125,10 +126,11 @@ class ContactHelper:
         workphone = wd.find_element_by_name("work").get_attribute("value")
         mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
         secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
+        address = wd.find_element_by_name("address").get_attribute("value")
         email = wd.find_element_by_name("email").get_attribute("value")
         email2 = wd.find_element_by_name("email2").get_attribute("value")
         email3 = wd.find_element_by_name("email3").get_attribute("value")
-        return Contact(firstname=firstname, lastname=lastname, id=id,
+        return Contact(firstname=firstname, lastname=lastname, id=id, address=address,
                        homephone=homephone, workphone=workphone,
                        mobilephone=mobilephone, secondaryphone=secondaryphone, email=email, email2=email2, email3=email3)
 
