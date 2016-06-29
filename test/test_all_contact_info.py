afab706ed1b5a131faf09d01e9f2eb2dfd665b4f
dariansk <103203@gmail.com>
2016-06-29 19:31:41 +0600

    Добавлен выбор случайного контакта в тесте информации о контакте

--- test/test_all_contact_info.py
+++ test/test_all_contact_info.py
@@ -1,9 +1,15 @@
+from model.contact import Contact
+from random import randrange
 import re
 
 
 def test_all_contact_info(app):
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    contact_from_home_page = app.contact.get_contact_list()[0]
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="testcontact", address="New York 24626", homephone="26265", workphone="997896789", mobilephone="97345632487"))
+    old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    contact_from_home_page = app.contact.get_contact_list()[index]
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.address == contact_from_edit_page.address
